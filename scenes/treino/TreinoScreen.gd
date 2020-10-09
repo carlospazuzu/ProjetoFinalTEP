c9abb71c0ed9d8bbf6704f7383df588070c432e1
Carlos Pazuzu <carlosh555@hotmail.com>
2020-10-09 12:57:19 -0300

    accelerometer tests

--- scenes/treino/TreinoScreen.gd
+++ scenes/treino/TreinoScreen.gd
@@ -4,24 +4,50 @@ extends Node2D
 var has_started: bool = false
 var current_time: int = 3
 
+var is_in_right_position: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 func _input(event):
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton:	
+		show_help_image()
+		# start_countdown()	
 		pass
 		# emit_signal()
 
 
-func _on_TouchScreenButton_pressed():
+func _process(delta):
+	if Input.get_accelerometer().x >= -10 and Input.get_accelerometer().x <= -8:
+		is_in_right_position = true
+	else:
+		is_in_right_position = false
+
+	if is_in_right_position:
+		start_countdown()
+		
+	var acc = Input.get_accelerometer()
+		
+	$AccelerometerHelper.text = 'X = ' + str(acc.x) + ' Y = ' + str(acc.y) + ' Z = ' + str(acc.z)
+
+func show_help_image():
+	$PauDaPlacaTreino.visible = false
+	$TreinarTouchButton.visible = false
+	$HelpImage.visible = true
+
+
+func start_countdown():
 	$CountdownTimer.start()
-	if not has_started:
-		$PauDaPlacaTreino.visible = false
-		$TreinarTouchButton.visible = false
+	if not has_started:		
 		$CountDownImage.texture = load('res://resources/others/Cont3.png')
 		has_started = true
+
+
+func _on_TouchScreenButton_pressed():	
+	show_help_image()
+	# start_countdown()
 	pass # Replace with function body.
 
 
